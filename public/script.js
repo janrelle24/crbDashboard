efc68bf81dd090f86e723e1757cfc129b95f38fc
janrelle24 <lubianojanrelle@gmail.com>
2026-01-02 21:11:42 +0800

    add script for open and close modal

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+/*start script for sidebar hamburger btn*/
 function initSidebar() {
     const sidebar = document.getElementById("sidebar");
     const hamburger = document.querySelector(".hamburger");
@@ -17,6 +18,29 @@ function initSidebar() {
         });
     });
 }
+/*end script for sidebar hamburger btn*/
+/*start script for news page modal*/
+const modal = document.getElementById("newsModal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtns = document.querySelectorAll(".close-modal"); 
+const tableBody = document.getElementById("newsTableBody");
+const form = document.getElementById("newsForm");
+const modalTitle = document.getElementById("modalTitle");
+
+let news = JSON.parse(localStorage.getItem("news")) || [];
+
+/* Modal controls */
+openModalBtn.onclick = () =>{
+    form.reset();
+    document.getElementById("newsId").value = "";
+    modalTitle.textContent = "Post News";
+    modal.classList.add("show");
+};
+
+closeModalBtns.forEach(btn => {
+    btn.onclick = () => modal.classList.remove("show");
+});
+/*end script for news page modal*/
 
 
 
